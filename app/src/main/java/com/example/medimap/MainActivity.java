e68672906e2d5d2a0a6eadf3415a3e86b1f0a837
hanan hijaze <hananhij11@gmail.com>
2024-08-27 21:18:13 +0300

    intents

--- app/src/main/java/com/example/medimap/MainActivity.java
+++ app/src/main/java/com/example/medimap/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.medimap;
 
 import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -26,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            createShortcut();
+        }
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             Intent in = new Intent(MainActivity.this, Home.class);
             startActivity(in);
@@ -37,6 +45,22 @@ public class MainActivity extends AppCompatActivity {
                 .asGif()
                 .load(R.drawable.loading)
                 .into(imageView);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+    private void createShortcut() {
+        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+
+        if (shortcutManager != null && shortcutManager.isRequestPinShortcutSupported()) {
+            ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "shortcut_example")
+                    .setShortLabel(getString(R.string.shortcut_short_label))
+                    .setLongLabel(getString(R.string.shortcut_long_label))
+                    .setIcon(Icon.createWithResource(this, R.drawable.ic_shortcut))
+                    .setIntent(new Intent(Intent.ACTION_VIEW,
+                            null, this, Home.class))
+                    .build();
+
+            shortcutManager.requestPinShortcut(shortcut, null);
+        }
     }
 
 }
