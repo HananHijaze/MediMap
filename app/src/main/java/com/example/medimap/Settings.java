0ebaca93b63a22fc941bb031d9f5246624b6d100
Majd Geath <majdgeath@gmail.com>
2024-08-29 09:04:29 +0300

    hanan is the best

--- app/src/main/java/com/example/medimap/Settings.java
+++ app/src/main/java/com/example/medimap/Settings.java
@@ -1,8 +1,6 @@
 package com.example.medimap;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Gender extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_gender);
+        setContentView(R.layout.activity_settings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        Button nextButton = findViewById(R.id.nextButton);
-        nextButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, height.class);
-            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/medimap/WorkOutTime.java
+++ app/src/main/java/com/example/medimap/WorkOutTime.java
@@ -1,6 +1,8 @@
 package com.example.medimap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,11 @@ public class WorkOutTime extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TrainingDays.class);
+            startActivity(intent);
         });
     }
 }
